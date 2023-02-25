d328e822e7f39349233fd9e337139c7a52fd1cc5
momotvit <110504519+momotvit@users.noreply.github.com>
2023-02-25 01:17:57 -0800

    task 3 is completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,67 @@
 
+import throttle from 'lodash.throttle';
+
+
+
+
+const form = document.querySelector('form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+let objectToBeSubmitedToLocalStorage = {email: "", message: ""};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInputLocalStorageDataChange, 500));
+
+
+function onFormSubmit(event) { 
+    event.preventDefault();
+
+    if (email.value === '' || message.value === '') {
+        return alert('Fill all of the fields, please');
+    }
+
+    objectToBeSubmitedToLocalStorage = {email: email.value, message: message.value};
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    email.value = "";
+    message.value = "";
+    form.reset(); 
+}
+
+function onInputLocalStorageDataChange(event) {
+    const { name, value } = event.target;
+    objectToBeSubmitedToLocalStorage[name] = value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToBeSubmitedToLocalStorage));
+}
+
+function loadDataFromLocalStorage() {
+    try {
+
+        const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+        return savedData === null ? undefined : JSON.parse(savedData);
+        
+    } catch (error) {
+
+        console.error('Get state error: ', error.message);
+    }
+}
+
+function uploadDataFromLocalStorageToTextArea() { 
+    const storageData = loadDataFromLocalStorage();
+        if (storageData) {
+            email.value = storageData.email;
+            message.value = storageData.message;
+        }
+
+    return storageData;
+}   
+
+uploadDataFromLocalStorageToTextArea()
+
+
+
+
+
