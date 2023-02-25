ff42a73d1c496b2797f092d0836a4f1ce2058dc3
momotvit <110504519+momotvit@users.noreply.github.com>
2023-02-24 19:04:44 -0800

    task 1 is completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+
+  
+////adding items to html element
+
+
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => 
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+//////// simpleLightbox hook up
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: 'true',  
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionAnimation: 'fade',
+    captionAnimationSpeed: 500,
+});
