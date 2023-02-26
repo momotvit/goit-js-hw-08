15810475e304a4b548b80fd62637e867a2748a77
momotvit <110504519+momotvit@users.noreply.github.com>
2023-02-26 11:28:47 -0800

    task 3 is updated

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,72 +1,43 @@
 
+
 import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let localStorageDataChange = { email: emailInput.value, message: messageInput.value };
 
 
-
-const form = document.querySelector('form');
-const email = document.querySelector('input[name="email"]');
-const message = document.querySelector('textarea[name="message"]');
-
-
-const LOCALSTORAGE_KEY = "feedback-form-state";
-
-let objectToBeSubmitedToLocalStorage = {email: "", message: ""};
-
-form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onInputLocalStorageDataChange, 500));
+const onInputLocalStorageDataChange = throttle(() => {
+    localStorageDataChange = { email: emailInput.value, message: messageInput.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(localStorageDataChange));
+}, 500);
 
 
-function onFormSubmit(event) { 
-    event.preventDefault();
+function uploadDataFromLocalStorageToTextArea(){
+    const dataFromLocStor = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (dataFromLocStor) {
+        const { email, message } = JSON.parse(dataFromLocStor);
+        emailInput.value = email;
+        messageInput.value = message;
+    }
+    return dataFromLocStor;
+};
 
-    if (email.value === '' || message.value === '') {
+function onFormSubmit(event){
+    
+    if (emailInput.value === '' || messageInput.value === '') {
         return alert('Fill all of the fields, please');
     }
-
-    objectToBeSubmitedToLocalStorage = {email: email.value, message: message.value};
+    event.preventDefault();
+    onInputLocalStorageDataChange();
+    console.log(localStorageDataChange);
     localStorage.removeItem(LOCALSTORAGE_KEY);
-    email.value = "";
-    message.value = "";
-    form.reset(); 
-}
+    emailInput.value = '';
+    messageInput.value = '';
+};
 
-function onInputLocalStorageDataChange(event) {
-    const { name, value } = event.target;
-    objectToBeSubmitedToLocalStorage[name] = value;
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToBeSubmitedToLocalStorage));
-}
-
-function loadDataFromLocalStorage() {
-    try {
-
-        const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
-        return savedData === null ? undefined : JSON.parse(savedData);
-        
-    } catch (error) {
-
-        console.error('Get state error: ', error.message);
-    }
-}
-
-function uploadDataFromLocalStorageToTextArea() { 
-    const storageData = loadDataFromLocalStorage();
-        if (storageData) {
-            email.value = storageData.email;
-            message.value = storageData.message;
-        }
-
-    return storageData;
-}   
-
-uploadDataFromLocalStorageToTextArea()
-
-
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        onFormSubmit(event);
-    }
-});
-
-
-
+form.addEventListener('input',onInputLocalStorageDataChange);
+form.addEventListener('submit', onFormSubmit);
+window.addEventListener('load', uploadDataFromLocalStorageToTextArea);
